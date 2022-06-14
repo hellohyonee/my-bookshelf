9560d86f132e7e321bd6163fca0f5f75d8733ecc
hellohyonee <hellohk07@gmail.com>
2022-06-15 00:23:04 +0900

    ErrorBoundary를 사용. Error 라우팅 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,28 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { ErrorBoundary } from 'react-error-boundary';
 import Home from './pages/Home';
 import Signin from './pages/Signin';
 import Add from './pages/Add';
 import Detail from './pages/Detail';
 import Edit from './pages/Edit';
 import NotFound from './pages/NotFound';
+import Error from './pages/Error';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/signin" element={<Signin />} />
-        <Route path="/add" element={<Add />} />
-        <Route path="/book/:id" element={<Detail />} />
-        <Route path="/edit/:id" element={<Edit />} />
-        <Route path="/*" element={<NotFound />} />
-      </Routes>
-    </BrowserRouter>
+    <ErrorBoundary FallbackComponent={Error}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/signin" element={<Signin />} />
+          <Route path="/add" element={<Add />} />
+          <Route path="/book/:id" element={<Detail />} />
+          <Route path="/edit/:id" element={<Edit />} />
+          <Route path="/*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </ErrorBoundary>
   );
 }
 
