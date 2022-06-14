f6cce8940a0be72476a7ce8ed7906cd35ad5689b
hellohyonee <hellohk07@gmail.com>
2022-06-14 23:31:51 +0900

    라우팅(Home/Signin/Add/Detail/Edit/NotFound)

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,22 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
+import Signin from './pages/Signin';
+import Add from './pages/Add';
+import Detail from './pages/Detail';
+import Edit from './pages/Edit';
+import NotFound from './pages/NotFound';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/*" element={<Home />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/signin" element={<Signin />} />
+        <Route path="/add" element={<Add />} />
+        <Route path="/book/:id" element={<Detail />} />
+        <Route path="/edit/:id" element={<Edit />} />
+        <Route path="/*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
   );
